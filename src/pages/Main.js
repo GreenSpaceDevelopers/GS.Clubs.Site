802d1b009d64a660244af0cea7fbb0d0438bc025
namol4anke <r.efremenko44@mail.ru>
2024-01-30 21:00:59 +0300

    fix messages

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,26 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './Main.css';
 import Slider from '../components/Slider';
 
+const messages = [
+    'Киберспорт в сердце Северной столицы!',
+    'Где сердце Северной столицы бьется в ритме киберспорта!',
+    'Под звездами Петербурга рождаются киберлегенды!',
+    'Твой киберспортивный путь начинается с северной столицы!'
+];
+
 function Main() {
     const [currentMessage, setCurrentMessage] = useState('Киберспорт в сердце Северной столицы!');
     const [index, setIndex] = useState(0);
-    
-    const messages = [
-        'Киберспорт в сердце Северной столицы!', 
-        'Где сердце Северной столицы бьется в ритме киберспорта!', 
-        'Под звездами Петербурга рождаются киберлегенды!', 
-        'Твой киберспортивный путь начинается с северной столицы!'
-    ];
-    
+
     useEffect(() => {
-    const intervalId = setInterval(() => {
-        const nextIndex = (index + 1) % messages.length;
-        setIndex(nextIndex);
-        setCurrentMessage(messages[nextIndex]);
-    }, 5000);
-    
-    return () => clearInterval(intervalId);
-    }, [index, messages]);
+        const intervalId = setInterval(() => {
+            const nextIndex = (index + 1) % messages.length;
+            setIndex(nextIndex);
+            setCurrentMessage(messages[nextIndex]);
+        }, 5000);
+
+        return () => clearInterval(intervalId);
+    }, [index]);
     
     return (
         <div className='mainContainer'>
